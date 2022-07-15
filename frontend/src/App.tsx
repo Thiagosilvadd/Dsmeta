5940199472d7cd008dc5c74498f142f0bbf62c34
Thiago Silva <silvaa.thiagod2@gmail.com>
2022-07-15 09:25:19 -0300

    Fist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import NotificationButton from "./components/notificationButton";
+
 function App() {
-  return <h1>OLá mundo</h1>;
+  return (
+    <>
+      <h1>OLá mundo</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
