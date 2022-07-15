b383d9b674f2b57492270b997120ed681158194b
Thiago Silva <silvaa.thiagod2@gmail.com>
2022-07-15 10:10:26 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NotificationButton from "./components/notificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>OLá mundo</h1>
-      <NotificationButton />
+      <Header />
+        <main>
+            <section id="sales">
+                <div className="dsmeta-container">
+                    <SalesCard />
+                </div>
+            </section>
+        </main>
+
     </>
   );
 }
