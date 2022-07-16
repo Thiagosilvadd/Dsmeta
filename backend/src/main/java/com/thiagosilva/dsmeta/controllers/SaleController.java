2df6f279b0eb5de2864ea90133b290a51eff3450
Thiago Silva <silvaa.thiagod2@gmail.com>
2022-07-16 09:50:44 -0300

    Twilio SMS

--- backend/src/main/java/com/thiagosilva/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/thiagosilva/dsmeta/controllers/SaleController.java
@@ -1,14 +1,12 @@
 package com.thiagosilva.dsmeta.controllers;
 
 import com.thiagosilva.dsmeta.entities.Sale;
+import com.thiagosilva.dsmeta.services.NotificationService;
 import com.thiagosilva.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,6 +16,9 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private NotificationService notificationService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
@@ -25,4 +26,10 @@ public class SaleController {
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
     }
+
+    @GetMapping("/{id}/notification")
+    public void notificationSms(@PathVariable Long id){
+        notificationService.sendSms(id);
+    }
+
 }
--- backend/src/main/java/com/thiagosilva/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/thiagosilva/dsmeta/repositories/SaleRepository.java
@@ -11,8 +11,6 @@ import java.time.LocalDate;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
-    // JPQL
     @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
     Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
